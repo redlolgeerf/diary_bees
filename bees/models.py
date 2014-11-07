8515b326890331765b3716ec82abcdf8160f1853
eyeinthebrick <mihanne@list.ru>
2014-11-07 23:52:08 +0300

    created models

--- bees/models.py
+++ bees/models.py
@@ -1,3 +1,44 @@
-from django.db import models
+# -*- coding: utf-8 -*-
 
-# Create your models here.
+from django.db import models
+from django.utils.translation import ugettext as _
+
+class DUser(models.Model):
+
+    d_login = models.CharField(verbose_name=_('diary_user_name'), max_length=255,
+                                blank=True, unique=True)
+    joined = models.DateTimeField(verbose_name=_('joined'), auto_now_add=True,
+                                editable=False)
+    last_visited = models.DateTimeField(verbose_name=_('last_visited'), blank=True,
+                                editable=False)
+    bees = models.TextField(verbose_name=_('bees'), blank=True)
+
+    def __str__(self):
+        return self.d_login
+
+    class Meta:
+        verbose_name = _('diary user')
+        verbose_name_plural = _('diary users')
+
+class History(models.Model):
+
+    action_choices = (
+                      ('l', _('left')),
+                      ('j', _('joined')),
+                      ('r', _('renamed')),
+                     )
+    d_id = models.IntegerField()
+    d_name = models.CharField(verbose_name=_('diary_user_name'), max_length=255)
+    action = models.CharField(choices=action_choices, max_length=25)
+    when = models.DateTimeField(verbose_name=_('when'), auto_now_add=True,
+                                editable=False)
+    duser = models.ForeignKey('Duser')
+
+    def __str__(self):
+        return '{} {}'.format(self.d_name, self.action)
+
+    class Meta:
+        verbose_name = _('bee')
+        verbose_name_plural = _('bees')
+        ordering = ['-when']
+        order_with_respect_to = 'duser'
