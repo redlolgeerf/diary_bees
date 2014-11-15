151e0b9635f840cce100a64cea6c6cb1effa73fa
eyeinthebrick <mihanne@list.ru>
2014-11-15 18:12:14 +0300

    add validation, that d_id is only digits

--- bees/forms.py
+++ bees/forms.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django import forms
+from django.utils.translation import ugettext as _
 from bees.models import DUser
 
 
@@ -9,3 +10,11 @@ class DUserForm(forms.ModelForm):
     class Meta:
         model = DUser
         fields = ['d_id']
+
+    def clean_d_id(self):
+        data = self.cleaned_data['d_id']
+        data = data.strip()
+        if not data.isdigit():
+            raise forms.ValidationError(
+                    _('Diary id can be only numbers'))
+        return data
